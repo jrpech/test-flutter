6b87bfcb54eda7b8dfd9e1b6c60fd4496ecaf13c
jorge@codemint.com.mx <jorge@codemint.com.mx>
2021-02-04 18:08:44 -0600

    movie list screen

--- test_app/lib/screens/cartelera_screen.dart
+++ test_app/lib/screens/cartelera_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_app/blocs/authentication/authentication_bloc.dart';
+import 'package:test_app/blocs/movies/movies_bloc.dart';
+import 'package:test_app/widgets/movie_item.dart';
 
 class CarteleraScreen extends StatelessWidget {
   @override
@@ -18,7 +20,30 @@ class CarteleraScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Text("CARTELERA"),
+      body: BlocProvider(
+        create: (context) {
+          return MoviesBloc()..add(FetchMovies());
+        },
+        child: BlocBuilder<MoviesBloc, MoviesState>(builder: (context, state) {
+          if (state is MoviesLoaded) {
+            return ListView.builder(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemCount: state.movies.length,
+                itemBuilder: (context, index) {
+                  return MovieItem(state.movies[index]);
+                });
+          }
+
+          if (state is MoviesLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Text("");
+        }),
+      ),
     );
   }
 }
